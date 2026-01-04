03915e46ab6f76a358d9a4b0f2035b0818a173bb
Tanushree Ahir <81023203+tanushree-coder-girl@users.noreply.github.com>
2026-01-04 20:38:23 +0530

    Add Open Graph and Twitter image metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,12 +34,21 @@ export const metadata: Metadata = {
     url: defaultUrl,
     siteName: "Reviselyt",
     type: "website",
+    images: [
+    {
+      url: `public/og-image.jpg`,
+      width: 1200,
+      height: 630,
+      alt: "Reviselyt - AI Study & Interview Summarizer",
+    },
+  ],
   },
   twitter: {
     card: "summary_large_image",
     title: "Reviselyt",
     description:
       "AI-powered last-minute exam & interview revision tool.",
+    images: [`public/og-image.jpg`],
   },
 };
 
